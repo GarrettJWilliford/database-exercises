d39240dee5699efec98e5116d7f662e33f889dfb
Garrett Williford <garrett.j.williford@gmail.com>
2019-09-03 15:31:01 -0500

    BOTTOM_TEXT

---
+++ 3.3_tables_exercises.sql
@@ -0,0 +1,16 @@
+SHOW TABLES; 
+
+/* 
+5.There appears to be booleans, dates, strings, and integer 
+
+6. Salaries, dept_emp and dept_manager
+
+7. Employees
+
+8. Salaries and dept_emp
+
+9. The dept_manager table is the connection
+
+*/
+
+SHOW CREATE TABLE dept_manager;
